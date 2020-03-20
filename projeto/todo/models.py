43e479ac995098fcbd7d32d505c02cb8a043d14b
Vagner Nornberg <vsaller@yahoo.com>
2020-03-20 15:42:36 -0300

    adicionando_usuario_modelo

--- projeto/todo/models.py
+++ projeto/todo/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Tarefa(models.Model):
     descricao=models.CharField(max_length=200)
-    criado_por=models.IntegerField()#endereça pra um usuario unico
+    criado_por=models.ForeignKey(User, on_delete=models.CASCADE)
     concluida=models.BooleanField(default=False)
 
 

b21b053de3e877c1a0cfa520e860001fea8cb7b3
Vagner Nornberg <vsaller@yahoo.com>
2020-03-20 18:02:21 -0300

    adicionando permissoes

---
+++ projeto/todo/permissions.py
@@ -0,0 +1,14 @@
+from rest_framework import permissions
+
+class UsuarioExiste(permissions.BasePermission):
+    
+    def permissao_objeto(self, request, view, obj):
+        return obj==request.user or request.user.is_staff
+
+class UsuarioAdm(permissions.BasePermission):
+
+    def tem_permissao(self, request, view):
+        return request.user and request.user.is_staff
+    
+    def tem_permissao_obj(self, request, view, obj):
+        return request.user and request.is_staff
